b741998c9badaea8dc125062c81305fd45dbc696
AFNAN PC <mohammad afnan>
2022-04-10 02:05:23 +0500

    add detail com  10 apr

--- src/app/tourdetail/tourdetail.component.ts
+++ src/app/tourdetail/tourdetail.component.ts
@@ -9,9 +9,35 @@ import { Router } from '@angular/router';
 export class TourdetailComponent implements OnInit {
 
   constructor(private router: Router) { }
+  tourdetail = [
+    {
+      lable: "The Instanbul Turkey!",
+      rating: "4.5",
+      image: "../../assets/images/turkey.png"
+    },
+    {
+      lable: "The Instanbul Turkey!",
+      rating: "4.5",
+      image: "../../assets/images/baku.png"
+    }, {
+      lable: "The Instanbul<br> Turkey!",
+      rating: "4.5",
+      image: "../../assets/images/Germany.png"
+    }, {
+      lable: "The Instanbul<br> Turkey!",
+      rating: "4.5",
+      image: "../../assets/images/maldive.png"
+    }, {
+      lable: "The Instanbul<br> Turkey!",
+      rating: "4.5",
+      image: "../../assets/images/pexels-travel1.jpg"
+    }
+  ]
+
 
   ngOnInit() {
   }
+
   backtohome() {
     this.router.navigate(['/dashboard'])
   }
