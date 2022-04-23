92e26978b94caabf8c831402a66db8df69462410
AFNAN PC <mohammad afnan>
2022-04-24 00:32:01 +0500

    loader added 24 april 2022

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,45 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { GlobalVariablesService } from '../services/global-variables.service';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { GlobalVariablesService } from "../services/global-variables.service";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
   logindata = [];
-
+  showloader: boolean = false;
   public profileForm: FormGroup;
-  constructor(private fb: FormBuilder, private router: Router, public global: GlobalVariablesService) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    public global: GlobalVariablesService
+  ) {
     this.profileForm = this.fb.group({
-      name: ['', Validators.required],
-      password: ['', Validators.required]
-    })
-
+      name: ["", Validators.required],
+      password: ["", Validators.required],
+    });
   }
 
-
   ngOnInit() {
-    this.global.currentRoute = this.router.routerState.snapshot.url
+    this.global.currentRoute = this.router.routerState.snapshot.url;
   }
 
   gotodashboard(data) {
-    this.router.navigate(['/dashboard'])
-    this.global.showSideBar = false
-    if (data.name == "afnan" && data.password == 'afnan123') {
+    if (data.name == "afnan" && data.password == "afnan123") {
       this.logindata.push(this.profileForm.value);
-      alert("login succecfully")
-      console.log(this.logindata)
+      alert("login succecfully");
+      this.showloader = true;
 
+      setTimeout(() => {
+        this.router.navigate(["/dashboard"]);
+      }, 5000);
+      this.global.showSideBar = false;
+      console.log(this.logindata);
+    } else {
+      alert("Please enter user name and password");
+      this.router.navigate(["/login"]);
     }
-    else {
-      alert("login failed")
-      this.router.navigate(['/login'])
-
-    }
-
   }
-
 }
