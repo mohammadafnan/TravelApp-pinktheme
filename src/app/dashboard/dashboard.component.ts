3a60d8fab9e3d6f9f1500c065c15dd7f365b701d
Afnan <mohammad.afnan@millsoft.com.pk>
2022-04-26 15:59:52 +0500

    add filter on dashbaord 26 april 2022

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,7 +12,9 @@ import { SidebarComponent } from "../sidebar/sidebar.component";
 
 export class DashboardComponent implements OnInit {
   sidebarshow: boolean = false;
-  tourdatacopy: any[] = [];
+  tourdatacopy: any[];
+  hoteldatacopy: any[];
+  busdatacopy: any[];
   constructor(private router: Router, public global: GlobalVariablesService) { }
   search: any;
   showloader: boolean = false;
@@ -25,25 +27,25 @@ export class DashboardComponent implements OnInit {
       image: "../../assets/images/baku.png",
     },
     {
-      id: 2,
+      id: 3,
       placename: "The Instanbul",
       country: "Turkey",
       image: "../../assets/images/turkey.png",
     },
     {
-      id: 2,
+      id: 3,
       placename: "The Maldive",
       country: "Maldive",
       image: "../../assets/images/maldive.png",
     },
     {
-      id: 3,
+      id: 2,
       placename: "The Germany",
       country: "Germany",
       image: "../../assets/images/Germany.png",
     },
     {
-      id: 3,
+      id: 2,
       placename: "The tree sunset",
       country: "Greece",
       image: "../../assets/images/pexels-travel1.jpg",
@@ -52,26 +54,31 @@ export class DashboardComponent implements OnInit {
 
   hotelData = [
     {
+      id: 1,
       placename: "The Hotel ",
       country: "Azerbaijan",
       image: "../../assets/images/hotel1.jpg",
     },
     {
+      id: 2,
       placename: "The Hotel",
       country: "Turkey",
       image: "../../assets/images/hotel2.jpg",
     },
     {
+      id: 2,
       placename: "The Hotel",
       country: "Maldive",
       image: "../../assets/images/hotel3.jpg",
     },
     {
+      id: 3,
       placename: "The Hotel",
       country: "Germany",
       image: "../../assets/images/hotel4.jpg",
     },
     {
+      id: 3,
       placename: "The Hotel",
       country: "Greece",
       image: "../../assets/images/hotel5.jpg",
@@ -79,26 +86,31 @@ export class DashboardComponent implements OnInit {
   ];
   busData = [
     {
+      id: 1,
       placename: "The Bus ",
       country: "Daewoo 1",
       image: "../../assets/images/bus1.jpeg",
     },
     {
+      id: 2,
       placename: "The Bus",
       country: "Daewoo 2",
       image: "../../assets/images/bus2.jpeg",
     },
     {
+      id: 2,
       placename: "The Bus",
       country: "Daewoo 3",
       image: "../../assets/images/bus3.jpeg",
     },
     {
+      id: 3,
       placename: "The Bus",
       country: "Daewoo 4",
       image: "../../assets/images/bus4.jpeg",
     },
     {
+      id: 3,
       placename: "The Bus",
       country: "Daewoo 5",
       image: "../../assets/images/bus5.jpeg",
@@ -108,6 +120,8 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.global.currentRoute = this.router.routerState.snapshot.url
     this.tourdatacopy = Object.assign([], this.tourData);
+    this.hoteldatacopy = Object.assign([], this.hotelData);
+    this.busdatacopy = Object.assign([], this.busData);
   }
 
 
@@ -131,19 +145,30 @@ export class DashboardComponent implements OnInit {
 
     if (newid == -1) {
       this.tourData = this.tourdatacopy;
+      this.hotelData = this.hoteldatacopy;
+      this.busData = this.busdatacopy;
+
       return;
     }
 
     this.tourData = this.tourdatacopy;
+    this.hotelData = this.hoteldatacopy;
+    this.busData = this.busdatacopy;
     this.showloader = true;
 
     let data = this.tourData.filter(a => a.id == newid);
+    // let data1 = this.hotelData.filter(a => a.id == newid);
+    // let data2 = this.busData.filter(a => a.id == newid);
+
     setTimeout(() => {
       this.showloader = false;
 
     }, 300);
 
     this.tourData = data;
+    // this.hotelData = data1;
+    // this.busData = data2;
+
 
   }
 
