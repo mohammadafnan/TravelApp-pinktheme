34257f8a47cfe4645dfb76701eec1c9175b48af2
AFNAN PC <mohammad afnan>
2022-05-24 01:31:30 +0500

    checkout page  ui working 22 may 2022

--- src/app/services/global-variables.service.ts
+++ src/app/services/global-variables.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from "@angular/core";
 })
 export class GlobalVariablesService {
   fightdata: any = [];
+  title: string[] = ["Mr", "Mrs", "Ms"];
   currentRoute: string = "";
   showSideBar: boolean = false;
   constructor() {}
