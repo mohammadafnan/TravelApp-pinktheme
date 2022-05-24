6606d61a6bae388f8d930bd8c3a1d9cafde8929c
AFNAN PC <mohammad afnan>
2022-05-25 00:01:15 +0500

    checkout page bank  btn working 22 may 2022

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -13,6 +13,7 @@ export class CheckoutComponent implements OnInit {
   public myform: FormGroup;
   showloader: boolean = false;
   show: string = "bank1";
+  indexno: number = -1;
 
   CheckoutData: any = [
     {
@@ -59,8 +60,7 @@ export class CheckoutComponent implements OnInit {
       cnic: ["", Validators.required],
       acountno: ["", Validators.required],
       title: ["", Validators.required],
-      bank: ["", Validators.required]
-
+      bank: ["", Validators.required],
     });
   }
 
@@ -78,12 +78,12 @@ export class CheckoutComponent implements OnInit {
       this.router.navigate(["/dashboard"]);
     }, 2000);
   }
-  indexno: any
 
   gotobank(list: any, i: any) {
-    // debugger
-    this.myform.get('bank').setValue(list.Bankname);
     this.indexno = i;
+    console.log(this.indexno);
+    // debugger
+    this.myform.get("bank").setValue(list.Bankname);
     console.log(this.global.fightdata);
     // debuger
   }
