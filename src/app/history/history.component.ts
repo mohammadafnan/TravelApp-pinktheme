c2529d1b50ffb8ed6f52b4d90face3f7aa678d74
Afnan <mohammad.afnan@millsoft.com.pk>
2022-06-08 15:36:52 +0500

    your tour top lable set 8 june 2022

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -8,15 +8,18 @@ import { GlobalVariablesService } from "../services/global-variables.service";
 })
 export class HistoryComponent implements OnInit {
   flightdataCopy: any = [];
-  constructor(public global:GlobalVariablesService) {}
+  constructor(public global: GlobalVariablesService) { }
 
   ngOnInit() {
     this.global.ishistory = true;
     this.global.isShowprofile = false
-    this.global.isHidden1=false;
+    this.global.isHidden1 = false;
+    this.global.isHidden = false;
+    this.global.isShowbackbtn = true;
 
     // JSON.parse(localStorage.getItem("Selected country name")); //retrieve the key
     this.flightdataCopy = JSON.parse(localStorage.getItem("flightdata"));
     console.log("flightdataCopy Data", this.flightdataCopy);
+
   }
 }
